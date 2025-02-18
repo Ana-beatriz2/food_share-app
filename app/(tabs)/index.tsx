2dd62d177b554696d788f862876709de4f3fa455
Ana-beatriz2 <caldas.ana9@gmail.com>
2025-02-18 00:46:08 -0300

    feat: implementa webview

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,21 @@
-import { Platform } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Platform, View, Text } from 'react-native';
 import { WebView } from 'react-native-webview';
-import React from 'react';
-import { View } from 'react-native';
+import { NetworkInfo } from 'react-native-network-info';
 
 export default function App() {
+  const [ipAddress, setIpAddress] = useState('');
+
+  useEffect(() => {
+    if (Platform.OS !== 'web') {
+      NetworkInfo.getIPAddress().then(ip => {
+        setIpAddress(ip);
+      });
+    }
+  }, []);
+
   const localUrl = 'http://localhost:5173';
-  const ipUrl = 'http://192.168.56.1:5173';
+  const ipUrl = `http://${ipAddress}:5173`; 
 
   const url = Platform.OS === 'web' ? localUrl : ipUrl;
 
